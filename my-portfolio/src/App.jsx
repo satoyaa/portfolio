118c494c58a142ec350969b321f8fabe18dbb7c4
satoyaa <108442382+satoyaa@users.noreply.github.com>
2026-05-02 15:16:57 +0900

    Add comment block for App component structure
    Added a comment block explaining the main structure of the App component.

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -7,6 +7,12 @@ import Contents from './components/Contents.jsx'
 import { AppContext } from './components/Context/Context.jsx';
 import Footer from './components/Footer.jsx'
 
+/*
+メイン部分
+ページ左にナビゲーション，右にコンテンツでナビゲーションを押すとコンテンツ部分が切り替わる
+フッターには各種SNS(qiita, git hub, instagram)へのリンクを記載
+*/
+
 function App() {
   const [pageFlag, setPageFlag] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
